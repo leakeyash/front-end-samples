bc49879b442f7f004d3ec09a80743e874724dd70
henry li <leakeyash@163.com>
2018-07-23 22:46:04 +0800

    add empty net core api

--- angular6sample-jobqueue/src/app/modules/job/job.module.ts
+++ angular6sample-jobqueue/src/app/modules/job/job.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JobQueueComponent } from '../../Components/job-queue/job-queue.component';
-
+import { PrimengModule } from '../primeng/primeng.module';
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    PrimengModule
   ],
   declarations: [JobQueueComponent]
 })
--- angular6sample-jobqueue/src/app/modules/primeng/primeng.module.ts
+++ angular6sample-jobqueue/src/app/modules/primeng/primeng.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { TableModule } from 'primeng/table';
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  exports:[
+    TableModule
+  ]
 })
 export class PrimengModule { }
