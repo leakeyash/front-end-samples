28917b42bc5dbaf42ed0f847233e1d71150f55d0
henry li <leakeyash@163.com>
2018-07-23 21:52:09 +0800

    initial commit for angular6 and primeNG sample

---
+++ angular6sample-jobqueue/src/app/modules/job/job.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { JobQueueComponent } from '../../Components/job-queue/job-queue.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [JobQueueComponent]
+})
+export class JobQueueModule { }
